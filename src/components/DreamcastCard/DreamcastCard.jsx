c4b0db8bcf4153e09106511312f41761c7c88cf8
Metranohm <andrewwink@gmail.com>
2022-10-25 14:57:36 -0500

    landing styling

---
+++ src/components/DreamcastCard/DreamcastCard.jsx
@@ -0,0 +1,10 @@
+import { useState, useEffect } from "react"
+import * as dreamcastService from '../../services/dreamcastService'
+
+const DreamcastCard = props => {
+  
+}
+
+
+
+export default DreamcastCard;
